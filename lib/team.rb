d0c70a014eb55873512e822665da9e289fed3aca
Divahar <divahar.manohar@railsdata.com>
2020-05-12 18:21:49 +0530

    v1.0_1205

--- lib/team.rb
+++ lib/team.rb
@@ -20,6 +20,24 @@ def login user, password
   click_login user, password
 end
 
+def default_role_edit
+  search_input_collaborator 'Admin'
+  if role_editor 'Admin'== nil
+    puts "Admin role doesn't have edit function"
+  end
+  search_input_collaborator 'Analyst'
+  a = role_editor 'Analyst'
+  if a == nil
+    puts "Analyst role doesn't have edit function"
+  end
+  search_input_collaborator 'Operator'
+  a = role_editor 'Operator'
+  if a == nil
+    puts "Operator role doesn't have edit function"
+  end
+
+
+end
 
 
 ################################################################
@@ -27,7 +45,7 @@ end
 ################################################################
 
 def navigate_to_Team_section
-  sleep 1
+  sleep 2
   find(:xpath,"//span[contains(text(),'Tools')]").click
   sleep 1
   click_link 'Team'
@@ -50,9 +68,24 @@ def search_input_collaborator user
   find(:xpath,"//input").send_keys user
 end
 
-################################################################
-########## -------------- API Response -------------- ##########
-################################################################
+def navigate_to_roles_section
+  sleep 2
+  find(:xpath,"//a[@href='#roles']").click
+end
+
+def role_name_default_check
+  "Admin"     ==  find(:xpath,"//div[contains(@class,'team-roles__cell')]/span[text()='Admin']").text
+  "Analyst"   ==  find(:xpath,"//div[contains(@class,'team-roles__cell')]/span[text()='Analyst']").text
+  "Operator"  ==  find(:xpath,"//div[contains(@class,'team-roles__cell')]/span[text()='Operator']").text
+  puts 'Admin, Analyst and Operator roles are available'
+end
+
+def role_editor user
+  sleep 1
+  find(:xpath,"(//span[text()='#{user}']/following::w-svg-icon[@iconid='pencil'])[1]").click
+end
+
+
 
 
 ################## FeaturesHelper ##################
--- spec/Team_functionality_TCs.rb
+++ spec/Team_functionality_TCs.rb
@@ -14,26 +14,44 @@ describe "Team functionality" do
 		nil
 	end
 
-	xit "TC016_Verify account admin is able to see an existing Collaborator’s last active status." do
+	it "TC004_Verify whether the default roles of Analyst, Admin and Operator are available in Roles section" do
 		navigate_to_Team_section
-		search_collaborators 'yogesh'
+		navigate_to_roles_section
+		role_name_default_check
 	end
 
-	xit "TC017_Verify account admin is able to search a particular collaborator with valid details." do
+	it "TC007_Verify account admin is not able to edit any of these Analyst, Admin and Operator roles" do
 		navigate_to_Team_section
-		search_collaborators 'yogesh'
+		navigate_to_roles_section
+		default_role_edit
 	end
 
-	it "TC015_Verify account admin is able to remove an existing Collaborator using delete icon." do
+	it "TC012_Verify account admin is able to see role created date and time" do
+		navigate_to_Team_section
+		navigate_to_roles_section
+		default_role_edit
+	end
+
+	xit "TC014_Verify account admin is able to change the role type for an existing Collaborator using edit icon." do
+		navigate_to_Team_section
+		search_collaborators 'divahar'
+		click_edit name
+	end
+
+	xit "TC015_Verify account admin is able to remove an existing Collaborator using delete icon." do
 		navigate_to_Team_section
 		search_collaborators 'divahar'
 		click_delete
 	end
 
-	it "TC014_Verify account admin is able to change the role type for an existing Collaborator using edit icon." do
+	xit "TC016_Verify account admin is able to see an existing Collaborator’s last active status." do
 		navigate_to_Team_section
 		search_collaborators 'divahar'
-		click_edit name
+	end
+
+	xit "TC017_Verify account admin is able to search a particular collaborator with valid details." do
+		navigate_to_Team_section
+		search_collaborators 'divahar'
 	end
 
 end
